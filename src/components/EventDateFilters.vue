1de57e3e778e0bd419f128360184888730822950
Tim Sandberg <tasandberg@gmail.com>
2017-04-02 23:25:19 -0700

    Tweak behavior of EventDateFilters to fit with style spec.

--- src/components/EventDateFilters.vue
+++ src/components/EventDateFilters.vue
@@ -1,10 +1,27 @@
 <template>
-  <div>
+  <div class='event-date-filters'>
     <h5 v-if="showTitle" class="filter-events-title">Date of event</h5>
     <div class="filter-events-date-picker">
-      <input ref="startDate" type="text" :value="startDate" />
-      <strong>to</strong>
-      <input ref="endDate" type="text" :value="endDate" />
+      <div class='date-picker-header'>
+        <input
+          autofocus
+          v-on:click="currentCalendar = 'startDate'"
+          ref="startDate"
+          type="text"
+          :value="startDate"
+        />
+        <strong>to</strong>
+        <input
+          v-on:click="currentCalendar = 'endDate'"
+          ref="endDate"
+          type="text"
+          :value="endDate"
+        />
+      </div>
+      <div id='calendar-container'>
+        <div v-show="currentCalendar === 'startDate'" id='start-calendar' />
+        <div v-show="currentCalendar === 'endDate'" id='end-calendar' />
+      </div>
     </div>
   </div>
 </template>
@@ -12,16 +29,22 @@
 <script>
 import { setHash } from 'src/util/url-hash';
 import moment from 'moment';
-
-// sigh
-window.moment = moment;
-
 import Pikaday from 'pikaday';
 
 // e.g., 2017-03-01
 const inputFormat = 'YYYY-MM-DD';
 // e.g., Mar 1, 2017
-const displayFormat = 'MMM D, YYYY'
+const displayFormat = 'MMM D, YYYY';
+
+// Pikaday needs all of these i18n values just to override the weekdays to
+// be single letters.  Everything is default except `weekdaysShort`
+const calendarFormat = {
+  previousMonth: 'Previous Month',
+  nextMonth: 'Next Month',
+  months: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+  weekdays: ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'],
+  weekdaysShort: ['S', 'M', 'T', 'W', 'T', 'F', 'S']
+}
 
 export default {
   name: 'event-type-filters',
@@ -34,42 +57,46 @@ export default {
     const endDate = this.filters.endDate ?
       moment(this.filters.endDate, inputFormat).format(displayFormat)
       : null;
-    
+
     return {
       startDate,
-      endDate
+      endDate,
+      currentCalendar: 'startDate'
     };
   },
 	mounted() {
     const component = this;
-
     this.pikadayStartDate = new Pikaday({
-      field: this.$refs.startDate,
-      format: displayFormat, 
-      position: 'bottom',
+      field: component.$refs.startDate,
+      format: displayFormat,
+      bound: false,
+      container: document.getElementById('start-calendar'),
+      i18n: calendarFormat,
       minDate: new Date(),
       onSelect: function() {
         const date = this.getMoment();
         component.startDate = date.format(displayFormat);
-        setHash({ startDate: date.format(inputFormat)  });
-
+        setHash({ startDate: date.format(inputFormat) });
         component.pikadayEndDate.setMinDate(date.toDate());
       }
     });
-
     this.pikadayEndDate = new Pikaday({
-      field: this.$refs.endDate,
-      format: displayFormat, 
-      position: 'bottom',
+      field: component.$refs.endDate,
+      format: displayFormat,
+      container: document.getElementById('end-calendar'),
+      i18n: calendarFormat,
       minDate: new Date(),
+      bound: false,
       onSelect: function() {
         const date = this.getMoment();
         component.endDate = date.format(displayFormat);
-        setHash({ endDate: date.format(inputFormat)  });
-
+        setHash({ endDate: date.format(inputFormat) });
         component.pikadayStartDate.setMaxDate(date.toDate());
       }
     });
+
+    // Focus the startDate field by default
+    this.$refs.startDate.focus();
 	}
 }
 </script>
